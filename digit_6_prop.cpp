0cc4337016bf68c26ff59aa403caf7361fcafabe
svalenti52 <svalenti52@yahoo.com>
2021-12-30 15:01:13 -0800

    Document digit_6_prop.cpp.

--- digit_6_prop.cpp
+++ digit_6_prop.cpp
@@ -2,6 +2,18 @@
 // Created by svalenti on 12/30/2021.
 //
 
+////
+//// International Mathematical Olympiad
+////        July 17, 2009
+//// IMO 1962 Problem 01
+////
+////    Find the smallest natural number n which has the following properties:
+////
+////    (a) Its decimal representation has 6 as the last digit.
+////    (b) If the last digit 6 is erased and placed in front of the remaining digits,
+////        the resulting number is four times as large as the original number n.
+////
+
 #include <iostream>
 #include <string>
 
